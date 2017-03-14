1b7a3c8008d96c451a2fa745d2b26f62ede733b8
TravisHelton <traviskhelton@gmail.com>
2017-03-14 19:55:51 -0400

    about to change my style

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var name="Travis";
+
+function checkUser(username){
+  if (username == "Travis"){
+   console.log("Welcome User");
+    }else{
+   console.log("Access Denied");
+    }
+}
+
+
+checkUser(name);
